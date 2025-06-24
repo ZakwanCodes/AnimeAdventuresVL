684a035867a9a01c461712309439b77d480c0933
Zakwan Dadabhai <zakwandadabhai@gmail.com>
2025-06-23 21:15:03 -0400

    header progress 1 - not done

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,18 @@ const express = require('express');
 
 const app = express();
 
+//It means all files inside your frontend folder are publicly accessible by the browser.
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+
 // Route for homepage
 app.get('/', (req, res) => {
   res.send("Hello there");
 });
+
+
+
+
 
 // Start the server on port 3000
 app.listen(3000, () => {
